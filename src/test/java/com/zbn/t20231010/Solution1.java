09572aaf8f412e2095ab747c9c6c82b42cb85360
zhaoburning <749932313@qq.com>
2023-11-21 20:24:26 +0800

    letcode解题

---
+++ src/test/java/com/zbn/t20231010/Solution1.java
@@ -0,0 +1,29 @@
+package com.zbn.t20231010;
+
+/**
+ * 1137. 第 N 个泰波那契数
+ * 泰波那契序列 Tn 定义如下：
+ *
+ * T0 = 0, T1 = 1, T2 = 1, 且在 n >= 0 的条件下 Tn+3 = Tn + Tn+1 + Tn+2
+ *
+ * 给你整数 n，请返回第 n 个泰波那契数 Tn 的值。
+ */
+public class Solution1 {
+    public int tribonacci(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n <= 2) {
+            return 1;
+        }
+        int p = 0, q = 0, r = 1, s = 1;
+        for (int i = 3; i <= n; ++i) {
+            p = q;
+            q = r;
+            r = s;
+            s = p + q + r;
+        }
+        return s;
+    }
+
+}
