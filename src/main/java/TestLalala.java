c7122390d7a013caeabac098720e6e44f1d1f67e
zhaoburning <749932313@qq.com>
2024-06-06 23:06:01 +0800

    两个数值交换，主要数值内存地址不能相同，否则会出错

--- src/main/java/TestLalala.java
+++ src/main/java/TestLalala.java
@@ -1,25 +1,32 @@
 /**
  * Created by lianxiang on 2017/5/7
  */
+
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Queue;
 import java.util.concurrent.*;
+
 public class TestLalala {
     //public static void main(String[] args) {
     //    BlockingQueue<String> queue = new LinkedBlockingQueue<String>();
     //    //ConcurrentLinkedQueue
     //}
     @Test
-    public void dd(){
+    public void dd() {
         byte[] bytes = new byte[1024 * 1024 * 62];
         System.out.println("ok");
     }
 
     @Test
     public void zz() throws Exception {
-
+        int a = 1;
+        int b = 2;
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+        System.out.println("a" + a + ",b" + b);
     }
 
     @Before
